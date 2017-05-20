e0e2775bee20cbfe593ec3c134bc30e97439210c
Dimitris Gazis <dgazis01@gmail.com>
2017-05-20 04:06:11 +0300

    Used the blade's assertion to get the actual urls for our scripts.

--- resources/views/layouts/partials/_scripts.blade.php
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -1,10 +1,11 @@
         <!-- Scripts -->
         <span>
                 <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-                <script>window.jQuery || document.write('<script src="../../public/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-                <script src="js/plugins.js"></script>
-                <script src="js/main.js"></script>
+                <script>window.jQuery || document.write('<script src="{{ asset('js/vendor/jquery-1.12.0.min.js') }}"><\/script>')</script>
+                <script src="{{ asset('js/plugins.js') }}"></script>
+                <script src="{{ asset('js/main.js') }}"></script>
+                <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js') }}"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-                <script src="js/vendor/modernizr-2.8.3.min.js"></script>
         </span>
+
