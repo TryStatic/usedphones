7c732f04e31e082c19aba6714568dda97e7ccfd9
Dimitris Gazis <dgazis01@gmail.com>
2017-05-26 12:47:53 +0300

    Filled in the view for showing individual listings.

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -19,29 +19,110 @@
 		margin-right: 10px;
 	}
 
+	h4 {
+		color: #005087;
+	}
+
+	h3 {
+		color: #007dd3;
+		text-decoration: underline;
+	}
+
+	p {
+		font-weight: bold;
+		margin-left: 10px;
+	}
+
+	ul {
+		font-weight: bold;
+	}
+
+	* {
+		word-wrap: break-word;
+	}
+
+	.inline {
+		display: inline;
+		font-size: 25px;
+	}
+
+	.cont {
+		background-color: rgba(201, 221, 255, .1);
+
+	}
 </style>
 
 <div class="panel panel-default" style="margin-top: 25px; margin-bottom: 25px">
-	<div class="row product-detail-2">
+	<div class="row cont" style="margin-top: 10px; margin-bottom: 10px">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 			<div class="wrapper">
 				<div class="row">
 					<div class="text-center" style="margin-top: 15px"> 
-						<img src="http://placehold.it/300x300">
+						<img class="img-thumbnail" src="http://placehold.it/400x300">
 					</div>
 				</div>
 				<div class="row">
-					<h1>{{ $listing->headline }}</h1>
+					<h2 class="text-center">{{ $listing->headline }}</h2>
 				</div>
+				<div class="row text-center">
+					<div class="inline"><span class="label label-primary">Featured</span></div>
+					<div class="inline"><span class="label label-default">32GB</span></div>
+					<div class="inline"><span class="label label-default">Mint</span></div>
+					<div class="inline"><span class="label label-default">Iphone</span></div>
+					<div class="inline"><span class="label label-default">Smartphone</span></div>
+				</div>
+				<br /><br />
 				<div class="row">
-					<div class="col-lg-6 text-left">
-						<div class="product-price">
-							<span>Harga</span><br/> Rp 1.365.000
-						</div>
-					</div>
-					<div class="col-lg-6 text-right">
-						<button type="button" class="btn btn-lg btn-success btn-lg">Find Agen</button>
-					</div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="http://placehold.it/150x150">
+				    </a>
+				  </div>
 				</div>
 			</div>
 		</div>
@@ -49,49 +130,82 @@
 			<div class="wrapper">
 				<div class="row">
 					<div class="col-lg-12"> 
-						<h3>Stok Barang</h3>
-						<div class="product-stock">
-							Tersedia 30 barang
-						</div>
-						<h3>Spesifikasi</h3>
-						<div class="product-specifications">
-							Kategori: Case & Cover
-							Brand: Lain-lain
-							Berat: 0.35 kg
-						</div>
-						<h3>Deskripsi Barang</h3>
-						<div class="product-descriptions">
+						<h3>Description</h3>
+						<div style="margin-left: 10px">
+							<h4>Product Description</h4>
 							<p>
-								Imak Flip Leather Cover Case Series for Samsung Galaxy S6 Edge<br/>
-								An Excellent protection for your smartphone<br/>
-								Imak Le Series untuk Samsung Galaxy S6 Edge. Le Series merupakan case<br/>
-								terbaru Imak yang terbuat dari bahan material kulit berkualitas untuk<br/>
-								mellindungi smartphone. Hadir dengan design corak yang elegan, case ini<br/>
-								juga dapat digunakan sebagai stand.<br/><br/>
-
-								Features<br/>
-								Fashion, Simple, Perfect<br/>
-								Imak Leather Case untuk Samsung Galaxy S6 Edge Inch hadir dengan design<br/>
-								yang ringan, tipis, serta modis untuk menambah kesan stylish bagi<br/>
-								smartphone Anda.<br/><br/>
-
-								Leather Materials bring best protection for your mobile phone<br/>
-								Imak Leather Case untuk Samsung Galaxy S6 Edge menggunakan bahan<br/>
-								kulit berkualitas untuk melindungi smartphone dari goresan, guncangan dan<br/>
-								kotoran lainnya<br/><br/>
-
-								The Surface is Dustproof, Anti fingerprint, and Easy To Clean<br/>
-								Imak Leather Case untuk Samsung Galaxy S6 Edge ini pun dilengkapi dengan<br/>
-								anti fingerprint, sehingga melindungi smartphone Anda dari bercakan tangan<br/>
-								yang mengganggu, serta dapat mencegah debu-debu kecil menempel.<br/>
-								Walaupun terdapat debu atau kotoran yang menempel, Anda dapat dengan<br/>
-								mudah membersihkannya.<br/><br/>
-
-								Easy Access to All Buttons<br/>
-								Anda tetap mendapat akses ke setiap port dan button yang terdapat di device<br/>
-								Anda dengan gampang.                            
+								{{ $listing->description }}
 							</p>
+							@if(!is_null($listing->dmgdescription))
+							<h4>Damage Description / Repair History</h4>
+							<p>
+								{{ $listing->dmgdescription }}
+							</p>
+							@endif
+							<h4>Overall Device Condition</h4>
+							@if($listing->condition == "mint")
+								<ul>
+									<li>Functions flawlessly</li>
+									<li>Perfect screen condition</li>
+									<li>No signs of use</li>
+								</ul>
+							@elseif($listing->condition == "good")
+								<ul>
+									<li>Functions flawlessly</li>
+									<li>Screen with little or no scratching</li>
+									<li>Few cosmetic defects such as light scratches</li>
+								</ul>
+							@elseif($listing->condition == "fair")
+								<ul>
+									<li>Functions flawlessly</li>
+									<li>Screen with noticeable scratching</li>
+									<li>Several cosmetic defects, scratches and/or dents</li>
+								</ul>
+							@elseif($listing->condition == "new")
+								<ul>
+									<li>Never activated, rooted, or refurbished</li>
+									<li>With original box and accessories</li>
+									<li>No signs of use</li>
+								</ul>
+							@endif
+
+							<h4>Additional Accessories</h4>
+							<ul>
+							@if($listing->boxincluded)
+								<li>Origial Box</li>
+							@endif
+							@if($listing->usbincluded)
+								<li>USB Cable</li>
+							@endif
+							@if($listing->adapterincluded)
+								<li>A/C Adapter</li>
+							@endif
+							@if($listing->microsdincluded)
+								<li>MicroUSD Card</li>
+							@endif
+							@if($listing->carchargedincluded)
+								<li>Car Charger</li>
+							@endif
+							</ul>
 						</div>
+						<h3>Additional Information</h3>
+						<ul>
+						@if($listing->originalowner)
+							<li>Original Owner: <span class="glyphicon glyphicon-ok"></li>
+						@else
+							<li>Original Owner: <span class="glyphicon glyphicon-remove"></li>
+						@endif
+
+						@if(!$listing->refurbished)
+							<li>Not Refurbished: <span class="glyphicon glyphicon-ok"></li>
+						@else
+							<li>Not Refurbished: <span class="glyphicon glyphicon-remove"></li>
+						@endif
+						<li>Device Color: {{ $listing->devicecolor }}</li>
+						<li>Device Storage: {{ $listing->devicestorage }}</li>
+						<li>Ship Location: {{ $listing->shiplocation }}</li>
+						</ul>
+					
 					</div>
 				</div>
 			</div>
